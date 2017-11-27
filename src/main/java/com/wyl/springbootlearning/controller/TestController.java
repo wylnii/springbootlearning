672c779975f68131b034a675cac41ad258f435f6
wylnii <wylnii@163.com>
2017-11-27 10:58:03 +0800

    添加 banner.txt

--- src/main/java/com/wyl/springbootlearning/controller/TestController.java
+++ src/main/java/com/wyl/springbootlearning/controller/TestController.java
@@ -2,7 +2,10 @@ package com.wyl.springbootlearning.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
